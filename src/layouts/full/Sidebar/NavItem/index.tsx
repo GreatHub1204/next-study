032e4e3f4377a6d591daef74d73fbc8f05596870
GreatHub1204 <grahasmin@gmail.com>
2023-08-27 22:26:09 +0900

    how to use the pathname

--- src/layouts/full/Sidebar/NavItem/index.tsx
+++ src/layouts/full/Sidebar/NavItem/index.tsx
@@ -11,13 +11,17 @@ type NavGroup = {
 }
 
 interface ItemType {
-    item:NavGroup;
+    item : NavGroup;
+     url : string;
 }
 
-const NavItem = ({item}:ItemType) => {
+
+const NavItem = ({item, url}:ItemType) => {
+    const isActive = url === item.href;
+
     return(
-        <div className="group mt-3 p-2 w-full hover:bg-[#5D87FF] rounded-[8px]">
-            <Link href={item.href} className="text-sm font-bold text-gray-500  group-hover:text-white">{item.title}</Link>
+        <div className={isActive?"group mt-3 p-2 w-full bg-[#5D87FF] rounded-[8px] cursor-pointer ": "group mt-3 p-2 w-full hover:bg-[#5D87FF] hover:bg-opacity-10 rounded-[8px] cursor-pointer "}>
+            <Link href={item.href} className={isActive?"text-sm font-bold text-white":"text-sm font-bold text-gray-500  group-hover:text-[#5D87FF]"}>{item.title}</Link>
         </div>        
     )
 }
--- src/layouts/full/Sidebar/SidebarItems.tsx
+++ src/layouts/full/Sidebar/SidebarItems.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import MenuItems from "./MenuItems";
 import { useRouter } from 'next/router';
+import { usePathname } from 'next/navigation';
+
 import NavItem from './NavItem';
 // import NavGroup from './NavGroup/NavGroup';
 
 
 const SidebarItems = ({toggleMobileSidebar}: any) => {
-    const {pathname } = useRouter();
+    // const {pathname } = useRouter();
     // const pathDirect = pathname;
-
+    const pathname = usePathname();
     return(
         <>
             {
                 MenuItems.map((item)=> {
                     return(
-                        <NavItem item={item}/>
+                        <NavItem item={ item } url={ pathname }/>
                     );
                 })
             }
